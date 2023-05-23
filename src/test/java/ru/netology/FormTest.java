f6a9e2aeed29e2a0c3d57e8ad12a2169720af998
Artyel <artyel@icloud.com>
2023-05-23 12:20:02 +0100

    Test added

--- src/test/java/ru/netology/FormTest.java
+++ src/test/java/ru/netology/FormTest.java
@@ -5,30 +5,51 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FormTest {
 
-        private WebDriver driver;
-        @BeforeAll
-        static void setUpAll() {
-            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        }
-        @BeforeEach
-        void setUp() {
-            driver = new ChromeDriver();
-        }
-        @AfterEach
-        void tearDown() {
-            driver.quit();
-            driver = null;
-        }
-        @Test
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
         //void shouldTestSomething() throws InterruptedException {
-            //throw new UnsupportedOperationException();
-          void shouldTestForm() {
-            driver.get(" http://localhost:7777");
-           // Thread.sleep(50000);
-        }
+        //throw new UnsupportedOperationException();
+    void shouldTestForm()  {
+        driver.get(" http://localhost:7777/");
+       // Thread.sleep(50000);
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Артур Тарусов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+44078830037");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[type=button]")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected,actual);
+    }
 }
